9446ac747159342b1a226f2d6473c646d30ccace
IrinaMirFOB <136357841+IrinaMirFOB@users.noreply.github.com>
2023-07-10 21:48:26 +0300

    Update issue-delete.cy.js

--- cypress/e2e/issue-delete.cy.js
+++ cypress/e2e/issue-delete.cy.js
@@ -32,4 +32,34 @@ describe('Issue details editing', () => {
 
     });
     });
+
+    it.only('Test 2: Create new test case for starting the deleting issue process, but cancelling this action', () => {
+
+      //Confirming that correct screen is opened, clicking on trash icon
+      cy.get('[placeholder="Short summary"]').contains('This is an issue of type: Task.');
+      cy.get('[data-testid="icon:trash"]').click();
+
+      //Working on dialog 
+      cy.get('[data-testid="modal:confirm"]').should("be.visible");
+      cy.get('[data-testid="modal:confirm"]').contains('Cancel').click();
+      cy.get('[data-testid="modal:confirm"]').should("not.exist");
+
+
+      cy.get('[placeholder="Short summary"]').contains('This is an issue of type: Task.');
+      //cy.get('[data-testid="icon:trash"]').next().contains('[data-testid="icon:close"]')
+
+      //Reload the page
+      cy.reload();
+
+      //Assert than only one list with name Backlog is visible and do steps inside of it
+      cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
+      //Assert that this list contains 4 issues and element specified text doesn't exist anymore
+      cy.get('[data-testid="list-issue"]')
+        .should('have.length', '4')
+        .first()
+        .find('p')
+        .contains('This is an issue of type: Task.').should('be.visible');
+
+  });
+  });
 });
